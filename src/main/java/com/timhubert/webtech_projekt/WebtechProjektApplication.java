771bb2f9482b20a941a021a7c28b7dad2c2ba223
TimHubert <Tim.Hubert@Student.HTW-Berlin.de>
2025-04-07 12:40:42 +0200

    generated project

---
+++ src/main/java/com/timhubert/webtech_projekt/WebtechProjektApplication.java
@@ -0,0 +1,13 @@
+package com.timhubert.webtech_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjektApplication.class, args);
+	}
+
+}
