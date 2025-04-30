c83a167509dccaabf46336832a4dacda6348cb8f
TimHubert <Tim.Hubert@Student.HTW-Berlin.de>
2025-04-30 20:16:29 +0200

    Add CORS config

--- vue-frontend/src/views/Tracker.vue
+++ vue-frontend/src/views/Tracker.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h1>Tracker</h1>
+    <GymTracker />
     <WorkoutView />
   </div>
 </template>
 
 <script setup>
 import WorkoutView from '../components/WorkoutView.vue'
+import GymTracker from "../components/GymTracker.vue";
 </script>
